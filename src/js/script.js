db81e5a02e026e08ea2d1fbf55c43ea682284658
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-11-04 23:25:58 +0530

    Info container on top: main logo + about cta

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,8 @@ Draggable.create(".draggable-content", {
   type: "x",
   bounds: document.querySelector(".main-container"),
   inertia: true,
+  dragClickables: false,
+  dragResistance: 0.25,
   onClick: function () {
     console.log("clicked");
   },
