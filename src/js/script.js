8d5e1be69e15439e15a9eef46ee1117e12c79c50
Anushka Chauhan <anushkachauhan1503@gmail.com>
2022-11-04 21:42:52 +0530

    Bug fix: Making the hotspots draggable with background image

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,9 @@ import { Draggable } from "gsap/Draggable";
 
 gsap.registerPlugin(Draggable);
 
-Draggable.create("#bg_image", {
+Draggable.create(".draggable-content", {
   type: "x",
-  bounds: document.querySelector(".image-container"),
+  bounds: document.querySelector(".main-container"),
   inertia: true,
   onClick: function () {
     console.log("clicked");
